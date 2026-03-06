3b7aaadcac96a500ef4f5f32676c0e470aca8d9d
IT Contractors Union <ITContractorsUnion@ITContractorsUnion.com>
2026-03-06 05:44:02 -0500

    Order By END_DATE Desc

--- Scripts/Find_State_H1B_Jobs.sh
+++ Scripts/Find_State_H1B_Jobs.sh
@@ -10,13 +10,14 @@ run_query () {
 q="select
 CASE_NUMBER,
 JOB_TITLE, 
-FULL_TIME_POSITION,
 EMPLOYER_NAME,
 SECONDARY_ENTITY_BUSINESS_NAME,
-EMPLOYER_POC_EMAIL,
-EMPLOYER_POC_PHONE,
+EMPLOYER_PHONE,
 EMPLOYER_POC_FIRST_NAME,
 EMPLOYER_POC_LAST_NAME,
+EMPLOYER_POC_EMAIL,
+EMPLOYER_POC_PHONE,
+EMPLOYER_POC_PHONE_EXT,
 WORKSITE_ADDRESS1,
 WORKSITE_ADDRESS2,
 WORKSITE_CITY,
@@ -36,13 +37,15 @@ CHANGE_PREVIOUS_EMPLOYMENT,
 NEW_CONCURRENT_EMPLOYMENT,
 CHANGE_EMPLOYER,
 AMENDED_PETITION,
+EMPLOYER_NAME,
 SOC_CODE,
 H_1B_DEPENDENT
 
-from LCA_Disclosure_Data_2022To2025Q4
+from LCA_Disclosure_Data_2022To2026Q1
 
 where
 (
+	CASE_STATUS = 'Certified' and
 	EMPLOYER_NAME ilike '%department%' or
 	SECONDARY_ENTITY_BUSINESS_NAME ilike '%department%' or
 	EMPLOYER_NAME ilike '%$name state %' or
@@ -53,11 +56,11 @@ where
 ) and
 
 WORKSITE_STATE = '$abbr'
+and END_DATE > now()
 
 order by 
-SECONDARY_ENTITY_BUSINESS_NAME,
-EMPLOYER_NAME,
-WORKSITE_COUNTY;"
+END_DATE desc,
+WORKSITE_COUNTY asc;"
 
 /usr/local/pgsql/bin/psql -A -d lca_data -c "$q" > ../State_H1B_Jobs/$fname"_State_H1B_Jobs.csv"
 /usr/local/pgsql/bin/psql -H -d lca_data -c "$q" > ../State_H1B_Jobs/$fname"_State_H1B_Jobs.html"
